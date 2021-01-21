5bdad5194dcf49687fd888f204592a826f843ae0
GrahamR5677 <77454624+GrahamR5677@users.noreply.github.com>
2021-01-21 16:59:39 +0000

    i have added a point system

--- main.ts
+++ main.ts
@@ -1,5 +1,25 @@
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+	
+})
 scene.setBackgroundColor(9)
-let you = sprites.create(img`
+let mySprite = sprites.create(img`
+    . . 4 4 4 . . . . 4 4 4 . . . . 
+    . 4 5 5 5 e . . e 5 5 5 4 . . . 
+    4 5 5 5 5 5 e e 5 5 5 5 5 4 . . 
+    4 5 5 4 4 5 5 5 5 4 4 5 5 4 . . 
+    e 5 4 4 5 5 5 5 5 5 4 4 5 e . . 
+    . e e 5 5 5 5 5 5 5 5 e e . . . 
+    . . e 5 f 5 5 5 5 f 5 e . . . . 
+    . . f 5 5 5 4 4 5 5 5 f . . f f 
+    . . f 4 5 5 f f 5 5 6 f . f 5 f 
+    . . . f 6 6 6 6 6 6 4 4 f 5 5 f 
+    . . . f 4 5 5 5 5 5 5 4 4 5 f . 
+    . . . f 5 5 5 5 5 4 5 5 f f . . 
+    . . . f 5 f f f 5 f f 5 f . . . 
+    . . . f f . . f f . . f f . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite)
+let chicken = sprites.create(img`
     ..........bbbbbb................
     .......bbb444444bb..............
     .....2244444ddd444b.............
@@ -33,39 +53,3 @@ let you = sprites.create(img`
     ..........................ccc...
     ................................
     `, SpriteKind.Food)
-let mySprite2 = sprites.create(img`
-    e e e . . . . e e e . . . . 
-    c d d c . . c d d c . . . . 
-    c b d d f f d d b c . . . . 
-    c 3 b d d b d b 3 c . . . . 
-    f b 3 d d d d 3 b f . . . . 
-    e d d d d d d d d e . . . . 
-    e d f d d d d f d e . b f b 
-    f d d f d d f d d f . f d f 
-    f b d d b b d d 2 f . f d f 
-    . f 2 2 2 2 2 2 b b f f d f 
-    . f b d d d d d d b b d b f 
-    . f d d d d d b d d f f f . 
-    . f d f f f d f f d f . . . 
-    . f f . . f f . . f f . . . 
-    `, SpriteKind.Player)
-let mySprite = sprites.create(img`
-    . . f f f . . . . . . . . . . . 
-    f f f c c . . . . . . . . f f f 
-    f f c c . . c c . . . f c b b c 
-    f f c 3 c c 3 c c f f b b b c . 
-    f f b 3 b c 3 b c f b b c c c . 
-    . c b b b b b b c f b c b c c . 
-    . c b b b b b b c b b c b b c . 
-    c b 1 b b b 1 b b b c c c b c . 
-    c b b b b b b b b c c c c c . . 
-    f b c b b b c b b b b f c . . . 
-    f b 1 f f f 1 b b b b f c c . . 
-    . f b b b b b b b b c f . . . . 
-    . . f b b b b b b c f . . . . . 
-    . . . f f f f f f f . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.Enemy)
-info.setScore(0)
-game.over(false)
