77c40cc993cff14a4977d91537b604727eae5367
GrahamR5677 <77454624+GrahamR5677@users.noreply.github.com>
2021-01-28 16:53:26 +0000

    i have added an enemy who kills you once overlapping him

--- main.ts
+++ main.ts
@@ -1,11 +1,20 @@
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
     info.changeScoreBy(1)
     chicken.setPosition(randint(0, 160), randint(0, 120))
-    info.startCountdown(10)
+    info.startCountdown(3)
+    mySprite.setStayInScreen(true)
+    info.setLife(3)
 })
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    myEnemy.setPosition(randint(0, 160), randint(0, 120))
+    game.over(false)
+    myEnemy.follow(chicken)
+})
+let myEnemy: Sprite = null
 let chicken: Sprite = null
+let mySprite: Sprite = null
 scene.setBackgroundColor(9)
-let mySprite = sprites.create(img`
+mySprite = sprites.create(img`
     . . 4 4 4 . . . . 4 4 4 . . . . 
     . 4 5 5 5 e . . e 5 5 5 4 . . . 
     4 5 5 5 5 5 e e 5 5 5 5 5 4 . . 
@@ -56,3 +65,21 @@ chicken = sprites.create(img`
     ..........................ccc...
     ................................
     `, SpriteKind.Food)
+myEnemy = sprites.create(img`
+    . f f f . . . . . . . . f f f . 
+    f f c . . . . . . . f c b b c . 
+    f c c . . . . . . f c b b c . . 
+    c f . . . . . . . f b c c c . . 
+    c f f . . . . . f f b b c c . . 
+    f f f c c . c c f b c b b c . . 
+    f f f c c c c c f b c c b c . . 
+    . f c 3 c c 3 b c b c c c . . . 
+    . c b 3 b c 3 b b c c c c . . . 
+    c c b b b b b b b b c c . . . . 
+    c b 1 b b b 1 b b b b f c . . . 
+    f b b b b b b b b b b f c c . . 
+    f b c b b b c b b b b f . . . . 
+    . f 1 f f f 1 b b b c f . . . . 
+    . . f b b b b b b c f . . . . . 
+    . . . f f f f f f f . . . . . . 
+    `, SpriteKind.Enemy)
